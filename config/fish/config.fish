9a82b8f0b20e05978d503ee10d16c78637c84cab
Konstantin Bauer <61027688+bauerei@users.noreply.github.com>
2020-10-23 10:15:35 +0200

    add alias for apt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,19 @@
-# Aliases
+# -------
+# aliases
+# -------
+# navigation
 alias ..="cd .."
-# In Ubuntu bat is installed as batcat
-alias cat=batcat
 
+# apt
+alias update="sudo apt update"
+alias upgrade="sudo apt upgrade"
+alias upgrate="sudo apt update && apt upgrade"
+
+# programs
+alias cat=batcat # in Ubuntu bat is installed as batcat
+
+# ---------
+# variables
+# ---------
 # vim as default text editor
 set -gx EDITOR vim
